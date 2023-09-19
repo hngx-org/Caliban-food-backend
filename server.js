8d03879db13dbc58c78e77146ea60614d78dff22
lenmorelms <learnmorelms@gmail.com>
2023-09-19 20:16:13 +0200

    reverse

--- server.js
+++ server.js
@@ -7,10 +7,3 @@ const server = createServer(app);
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-//Connect to database
-db.authenticate().then(() => {
-  console.log("Connected to the database successfully");
-}).catch(err => {
-  console.log("Failed to connect to the database:", err);
-});
