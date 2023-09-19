8f1268a7952a72fa34d1564645086b7609f3dea4
lenmorelms <learnmorelms@gmail.com>
2023-09-19 20:00:52 +0200

    Skeleton code for db connection

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const createServer = require("http").createServer;
 const app = require("./app");
+const db = require("./configs/dbConfig");
 const PORT = process.env.PORT || 7001;
 
 const server = createServer(app);
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+//Connect to database
+db.authenticate().then(() => {
+  console.log("Connected to the database successfully");
+}).catch(err => {
+  console.log("Failed to connect to the database:", err);
 });
