643345a16395a7e6d51efd50395aef39b6568a9c
ignatiusomeje <mrexcel153@gmail.com>
2023-09-20 09:54:08 +0100

    delete the lunches.js file. modify the controllers in the rewardControllers.js. add routes in the index.routes,js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,16 +1,23 @@
 const express = require("express");
 
-
+const {
+  addReward,
+  getAllReward,
+  getOneReward,
+  updateReward,
+} = require("./../controllers/rewardControllers");
 
 //Define the routes here.
 module.exports = function routes(app) {
-    app.use(express.json());
-  
-    // Registration & authentication routes.
-    //   app.use("/api/user", userRouter);
-    //   app.use("/api/auth", authRouter);
+  app.use(express.json());
 
-    // Any other routes
-   
+  // Registration & authentication routes.
+  //   app.use("/api/user", userRouter);
+  //   app.use("/api/auth", authRouter);
 
-}
+  // Any other routes
+  app.post("/api/lunch/send", addReward);
+  app.get("/api/lunch/all/", getAllReward);
+  app.get("/api/lunch/:id", getOneReward);
+  app.put("/api/lunch/redeem/:id", updateReward);
+};
