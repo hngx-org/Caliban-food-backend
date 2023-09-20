3c59a841a06a249a8e99267476e4e962ead2fd9e
Nosa <nosakhareaghedo42@gmail.com>
2023-09-20 22:37:27 +0000

    created the first stage for user registration

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require("passport");
+const passportSetup = require('../../config/passport-setup')
 const UserController = require("../../controllers/user")
 
 
-router.post("/signup", UserController.userSignup);
-
+router.get("/signup", passport.authenticate("google", {scope: ['profile', 'email']}));
+router.get('/redirect', passport.authenticate('google'), (req, res)=>{})
 module.exports = router;
