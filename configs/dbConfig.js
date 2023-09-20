c65775a3a7c16cb5a440049e826e871e20aa38de
Olaniyi Olabode <Olaniyiolabode99@yahoo.com>
2023-09-20 12:35:39 +0100

    no commit

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -1,27 +1,23 @@
-const Sequelize = require('sequelize')
-  
+const Sequelize = require("sequelize");
+
 // Creating new Object of Sequelize
-const sequelize = new Sequelize(
-    'reward-db',
-    'root',
-    'toor', {
-  
-        // Explicitly specifying 
-        // mysql database
-        dialect: 'mysql',
+const sequelize = new Sequelize("lunch_app", "root", "root", {
+  // Explicitly specifying
+  // mysql database
+  dialect: "mysql",
 
-        // So by default, host is 'localhost'           
-        host: 'localhost',
-        logging: console.warn // Enable logging
-    }
-);
+  // So by default, host is 'localhost'
+  host: "localhost",
+  logging: console.warn, // Enable logging
+});
 
-sequelize.authenticate()
-.then(() => {
-    console.log("Connection established")
-})
-.catch((err) => {
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection established");
+  })
+  .catch((err) => {
     console.log("error during connecting");
-})
-  
-module.exports = sequelize
+  });
+
+module.exports = sequelize;
--- controllers/rewardControllers.js
+++ controllers/rewardControllers.js
@@ -1,67 +1,53 @@
-const db = require('../models/index')
+const db = require("../models/index");
 
 // create main Model
-const Reward = db.models.reward
-
-
+const Reward = db.models.reward;
 
 // 1. create reward
 
 const addReward = async (req, res) => {
+  let info = {
+    senderId: req.body.senderId,
+    receiverId: req.body.receiverId,
+    quantity: req.body.quantity,
+    redeemed: req.body.redeemed,
+    note: req.body.note,
+  };
 
-    let info = {
-        senderId: req.body.senderId,
-        receiverId: req.body.receiverId,
-        quantity: req.body.quantity,
-        redeemed: req.body.redeemed,
-        note: req.body.note
-    }
-
-    const reward = await Reward.create(info)
-    res.status(200).send(reward)
-    console.log(reward)
-
-}
-
-
+  const reward = await Reward.create(info);
+  res.status(200).send(reward);
+  console.log(reward);
+};
 
 // 2. get all rewards
 
 const getAllReward = async (req, res) => {
-
-    let reward = await Reward.findAll({})
-    res.status(200).send(reward)
-
-}
+  let reward = await Reward.findAll({});
+  res.status(200).send(reward);
+};
 
 // 3. get single reward
 
 const getOneReward = async (req, res) => {
-
-    let id = req.params.id
-    let reward = await Reward.findOne({ where: { id: id } })
-    res.status(200).send(reward)
-
-}
+  let id = req.params.id;
+  let reward = await Reward.findOne({ where: { id: id } });
+  res.status(200).send(reward);
+};
 
 // 4. redeem reward
 // to change the boolen from true to false after the reward has been redeem
 
 const updateReward = async (req, res) => {
+  let id = req.params.id;
 
-    let id = req.params.id
+  const reward = await Reward.update(req.body, { where: { id: id } });
 
-    const reward = await Reward.update(req.body, { where: { id: id } })
-
-    res.status(200).send(reward)
-
-
-}
-
+  res.status(200).send(reward);
+};
 
 module.exports = {
-    addReward,
-    getAllReward,
-    getOneReward,
-    updateReward
-}
+  addReward,
+  getAllReward,
+  getOneReward,
+  updateReward,
+};
