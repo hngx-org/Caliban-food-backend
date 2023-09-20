75e8e29eb6c0034d608f31248b693d1983e61a28
jamesblack <jaymuritu@gmail.com>
2023-09-21 01:17:11 +0300

    Add comments to server.js to improve readability

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// Import the createServer function from the built-in "http" module
 const createServer = require("http").createServer;
+
+// Import the Express.js application instance from the "./app" module
 const app = require("./app");
+
+// Define the port number for the server to listen on, with a fallback to 7001 if not specified in the environment variables
 const PORT = process.env.PORT || 7001;
 
+// Create an HTTP server instance using the Express application
 const server = createServer(app);
+
+// Start the server and listen on the specified port
 server.listen(PORT, () => {
+  // Log a message indicating that the server is running and on which port
   console.log(`Server is running on port ${PORT}`);
 });
