3d3e98d000689e45e7374d2a56295d614b4100ca
Salama <malach.salama@gmail.com>
2023-09-21 22:47:29 +0300

    add findOne method to fetch a user in the getOneReward function

--- controllers/reward.js
+++ controllers/reward.js
@@ -1,4 +1,6 @@
 const { Lunches } = require("../models");
+const { User } = require("../models");
+const { Op } = require("sequelize");
 
 // create main Model
 const Reward = Lunches;
@@ -13,41 +15,41 @@ const Reward = Lunches;
 // 1. create reward
 // destructuring
 const addReward = async (req, res) => {
-    try {
-        const { senderId, receiverId, quantity, note } = req.body;
-        if (typeof quantity == 'string') {
-            return res.status(400).json({
-                message: "Invalid Quantity",
-                statusCode: 400,
-            });
-        }
-        if (parseInt(quantity) > 4) {
-            return res.status(400).json({
-                message: "Quantity is higher",
-                statusCode: 400,
-            });
-        }
-
-        const createReward = {
-            senderId,
-            receiverId,
-            quantity,
-            note,
-        };
-        await Reward.create(createReward);
-        res.status(201).json({
-            message: "Reward request created successfully",
-            statusCode: 201,
-            data: null,
-        });
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            message: `Error creating Reward: ${error?.message}`,
-            statusCode: 500,
-            data: null,
-        });
+  try {
+    const { senderId, receiverId, quantity, note } = req.body;
+    if (typeof quantity == "string") {
+      return res.status(400).json({
+        message: "Invalid Quantity",
+        statusCode: 400,
+      });
     }
+    if (parseInt(quantity) > 4) {
+      return res.status(400).json({
+        message: "Quantity is higher",
+        statusCode: 400,
+      });
+    }
+
+    const createReward = {
+      senderId,
+      receiverId,
+      quantity,
+      note,
+    };
+    await Reward.create(createReward);
+    res.status(201).json({
+      message: "Reward request created successfully",
+      statusCode: 201,
+      data: null,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: `Error creating Reward: ${error?.message}`,
+      statusCode: 500,
+      data: null,
+    });
+  }
 };
 
 /**
@@ -60,36 +62,35 @@ const addReward = async (req, res) => {
 // 2. get all rewards
 
 const getAllReward = async (req, res) => {
-    try {
-        const { id } = req.body.user;
-        let rewardReceived = await Reward.findAll({ where: { receiverId: id } });
-        let rewardSent = await Reward.findAll({ where: { senderId: id } });
+  try {
+    const { id } = req.body.user;
+    let rewardReceived = await Reward.findAll({ where: { receiverId: id } });
+    let rewardSent = await Reward.findAll({ where: { senderId: id } });
 
-        const reward = [...rewardReceived, ...rewardSent];
+    const reward = [...rewardReceived, ...rewardSent];
 
-        //check if any rewards exist
-        if (reward.length === 0) {
-            return res.status(404).json({
-                message: "No Reward for User",
-                statusCode: 404,
-                data: null,
-            });
-        }
-
-        // Respond with the fetched rewards
-        return res.status(200).json({
-            message: "Reward request fetched successfully",
-            statusCode: 200,
-            data: reward,
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            message: `Error fetching Reward: ${error?.message}`,
-            statusCode: 500,
-            data: null,
-        });
+    //check if any rewards exist
+    if (reward.length === 0) {
+      return res.status(404).json({
+        message: "No Reward for User",
+        statusCode: 404,
+        data: null,
+      });
     }
+
+    // Respond with the fetched rewards
+    return res.status(200).json({
+      message: "Reward request fetched successfully",
+      statusCode: 200,
+      data: reward,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: `Error fetching Reward: ${error?.message}`,
+      statusCode: 500,
+      data: null,
+    });
+  }
 };
 
 /**
@@ -102,35 +103,42 @@ const getAllReward = async (req, res) => {
 // 3. get single reward
 
 const getOneReward = async (req, res) => {
-    try {
-        const id = req.params.id;
-        let reward = await Reward.findOne({ where: { id: id } });
-        if (!reward) {
-            return res.status(404).json({
-                message: "No Reward Found",
-                statusCode: 404,
-                data: null,
-            });
-        }
-
-        return res.status(200).json({
-            message: "Reward fetched successfully",
-            statusCode: 200,
-            data: reward,
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: `Error fetching Reward: ${error?.message}`,
-            statusCode: 500,
-            data: null,
-        });
+  try {
+    const { id } = req.user;
+    const { org_id } = await User.findOne({ where: { id: id } });
+    const paramsId = req.params.id;
+    const reward = await Reward.findOne({
+      where: {
+        [Op.or]: [
+          { receiver_id: id, org_id, id: paramsId },
+          { sender_id: id, org_id, id: paramsId },
+        ],
+      },
+    });
+    if (!reward) {
+      return res.status(404).json({
+        message: "No Reward Found",
+        statusCode: 404,
+        data: null,
+      });
     }
+
+    return res.status(200).json({
+      message: "Reward fetched successfully",
+      statusCode: 200,
+      data: reward,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: `Error fetching Reward: ${error?.message}`,
+      statusCode: 500,
+      data: null,
+    });
+  }
 };
 
-
-
 module.exports = {
-    addReward,
-    getAllReward,
-    getOneReward
+  addReward,
+  getAllReward,
+  getOneReward,
 };
