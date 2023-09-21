d135b26e359bc32449b16e660cc75b77535bde31
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-09-21 17:45:39 +0100

    Update organization.js
    Signed-off-by: faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>

--- services/organization.js
+++ services/organization.js
@@ -1,7 +1,7 @@
 const { Organization } = require('../models');
 
 // Create a new organization
-const createOrganization = async (organizationData) => {
+/* const createOrganization = async (organizationData) => {
   try {
     const { organization_name, lunch_price } = organizationData;
 
@@ -14,8 +14,34 @@ const createOrganization = async (organizationData) => {
   } catch (error) {
     throw new Error('Error creating organization');
   }
+}; */
+
+// Update an existing organization
+const updateOrganization = async (orgId, updatedData) => {
+  try {
+    const updatedOrganization = await Organization.findByPk(orgId);
+
+    if (!updatedOrganization) {
+      throw new Error('Organization not found');
+    }
+
+    // Update the organization properties as needed
+    if (updatedData.organization_name) {
+      updatedOrganization.name = updatedData.organization_name;
+    }
+    if (updatedData.lunch_price) {
+      updatedOrganization.lunch_price = updatedData.lunch_price;
+    }
+
+    await updatedOrganization.save();
+
+    return updatedOrganization;
+  } catch (error) {
+    throw new Error('Error updating organization');
+  }
 };
 
 module.exports = {
   createOrganization,
+  updateOrganization, // Export the new service function for updating organizations
 };
