8a853d3a20aae0cbae64885323e06a76edbc0a7d
blac <badejo.semilore.7@gmail.com>
2023-09-21 14:59:32 +0100

    add withdraw routes to route /index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserRoutes = require('./user');
-const userInfo = require('./userInfo');
-const getAllUsers = require('./getAllUsers');
-const searchUser = require("./searchUsers")
+const UserRoutes = require("./user");
+const userInfo = require("./userInfo");
+const getAllUsers = require("./getAllUsers");
+const searchUser = require("./searchUsers");
+const withdrawRoutes = require("./withdraw");
 
-router.use('/', UserRoutes);
-router.use('/users', getAllUsers);
-router.use('/user', userInfo);
-router.use("/",searchUser);
+router.use("/", UserRoutes);
+router.use("/users", getAllUsers);
+router.use("/user", userInfo);
+router.use("/", searchUser);
+router.use("/", withdrawRoutes);
 
 module.exports = router;
