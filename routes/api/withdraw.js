dcdae9590a2f747f2276b224d101bd78db2863eb
apha-pesso <mutiuayo99@gmail.com>
2023-09-21 13:45:37 +0100

    Add the withdrawal route

---
+++ routes/api/withdraw.js
@@ -0,0 +1,14 @@
+// Withdrawal routes
+
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../../middleware/user");
+const withdrawalController = require("../../controllers/withdraw");
+
+router.post(
+  "/api/withdrawal/request",
+  authMiddleware,
+  withdrawalController.makeWithdrawal
+);
+
+module.exports = router;
