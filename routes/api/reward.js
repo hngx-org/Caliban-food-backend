275e5d20c432a9f662f23cbdeda4f5b97c8ddfe7
ignatiusomeje <mrexcel153@gmail.com>
2023-09-22 11:47:23 +0100

    update the user model with lunch_credit_balance: DataTypes.INTEGER. revert the branch to the previous and merge the main with the feature/sendlunch

--- routes/api/reward.js
+++ routes/api/reward.js
@@ -7,12 +7,10 @@ const {
   addReward,
   getAllReward,
   getOneReward,
-  redeemLunch
 } = require("../../controllers/reward");
 
 router.post("/send", authenticateToken, addReward);
 router.get("/all", authenticateToken, getAllReward);
 router.get("/:id", authenticateToken, getOneReward);
-router.put('/redeem/:id',authenticateToken, redeemLunch)
 
 module.exports = router;
