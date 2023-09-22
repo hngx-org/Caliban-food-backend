578763a0539c3f889f107221d03b086a14db61d6
Coder1967 <nosakhareaghedo42@gmail.com.com>
2023-09-22 14:44:22 +0100

    working on staff signup

--- routes/api/organization.js
+++ routes/api/organization.js
@@ -6,5 +6,35 @@ const authenticateToken = require('../../middleware/user');
 
 router.put('/create/:orgId', authenticateToken, orgController.createOrUpdateOrg);
 
+router.post("/staff/signup", async (req, res)=>{
+    try {
+          const  {email, password, otpToken, firstName, lastName, phoneNumber} = req.body
+
+          // Call the signupUser service function
+          const { user, token } = await signupUser({
+          email,
+          password,
+          orgId,
+          firstName,
+          lastName,
+      });
+    
+          const formattedUser = {
+          id: user.id,
+          email: user.email,
+          password_hash: user.password_hash,
+          org_id: user.org_id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+      };
+      
+    res.status(201).json({ success: true, user: formattedUser, token });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = router;
