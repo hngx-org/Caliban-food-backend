bad90cba8b6f4f5f08e9b27ef77c96195253fe34
Godsent Michael <gentlemotivator@gmail.com>
2023-09-22 14:15:38 +0100

    Add production db config env variables

--- config/config.js
+++ config/config.js
@@ -18,11 +18,12 @@ module.exports = {
   },
 
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
   },
 
   JWT_SECRET: process.env.JWT_SECRET,
