c6b631bbfaee493eb8ac7cbf9c0611af2f7d49bb
Coder1967 <nosakhareaghedo42@gmail.com.com>
2023-09-22 22:32:52 +0100

    improving error handling of staff signup

--- controllers/organization.js
+++ controllers/organization.js
@@ -2,6 +2,7 @@ const orgService = require('../services/organization');
 const signupStaff = require("../services/staff").signupStaff
 const jwt = require("jsonwebtoken")
 
+
 const createOrUpdateOrg = async (req, res) => {
     try {
         const { organization_name, lunch_price } = req.body;
@@ -39,18 +40,19 @@ const staffSignup = async (req, res)=>{
     try {
           const  {email, password, otp_token, first_name, last_name, phone_number} = req.body
 
-          if (!phone_number){
-            return res.status(400).json({message: "missing phone number"})
+      if (!phone_number){
+            return res.status(400).json({error: "missing phone number"})
           }
 	  if (!email){
-		  return res.status(400).json({message: "missing email"})
+		  return res.status(400).json({error: "missing email"})
           }
 	  if (!otp_token){
-		  return res.status(400).json({message: "missing otpToken"})}
+		  return res.status(400).json({error: "missing otp_token"})}
 	  if (!first_name){
-		  return res.status(400).json({message: "missing first_name"})
-          }if (!last_name){
-            return res.status(400).json({message: "missing last name"})
+		  return res.status(400).json({error: "missing first_name"})
+          }
+      if (!last_name){
+            return res.status(400).json({error: "missing last name"})
           }if (!password){
 		  return res.status(400).json({message: "missing password"})
 	  }
@@ -68,6 +70,7 @@ const staffSignup = async (req, res)=>{
           last_name,
           phone_number
       });
+      if (!user) return res.status(400).json({"error": "Email already in use"})
     
           const formattedUser = {
           id: user.id,
@@ -76,14 +79,14 @@ const staffSignup = async (req, res)=>{
           org_id: user.org_id,
           first_name: user.first_name,
           last_name: user.last_name,
-	  phone_number: user.phone_number,
+	      phone_number: user.phone_number,
           created_at: user.created_at,
           updated_at: user.updated_at,
       };
       
     res.status(201).json({ success: true, user: formattedUser});
   } catch (error) {
-    throw error;
+    res.status(403).json({"error": "invalid token"});
   }
 }
 
