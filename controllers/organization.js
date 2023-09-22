2e0338ea24e081e15bb4e4a87f2e03745e599a73
ignatiusomeje <mrexcel153@gmail.com>
2023-09-22 15:22:22 +0100

    Add the updateLunchPrize controller and routes

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,5 +1,8 @@
 const { validationResult } = require("express-validator");
 const orgService = require("../services/organization");
+const { Organization } = require("../models");
+const { User } = require("../models");
+const { string } = require("yargs");
 
 const createOrUpdateOrg = async (req, res) => {
   try {
@@ -15,12 +18,10 @@ const createOrUpdateOrg = async (req, res) => {
         lunch_price,
       });
 
-      return res
-        .status(200)
-        .json({
-          message: "Organization created or updated successfully",
-          updateOrg,
-        });
+      return res.status(200).json({
+        message: "Organization created or updated successfully",
+        updateOrg,
+      });
     } else {
       //if orgId is not provided it is treated as creation
       const createOrg = await orgService.createOrganization({
@@ -28,12 +29,10 @@ const createOrUpdateOrg = async (req, res) => {
         lunch_price,
       });
 
-      return res
-        .status(201)
-        .json({
-          message: "Organization created or updated successfully",
-          createOrg,
-        });
+      return res.status(201).json({
+        message: "Organization created or updated successfully",
+        createOrg,
+      });
     }
   } catch (err) {
     console.log("Error creating or updating organization", err);
@@ -42,15 +41,15 @@ const createOrUpdateOrg = async (req, res) => {
 };
 
 const sendOrganizationInvite = async (req, res) => {
-    const errors  =  validationResult(req.body)
-    if(!errors.isEmpty()){
-        throw new Error("invalid input")
-    }
+  const errors = validationResult(req.body);
+  if (!errors.isEmpty()) {
+    throw new Error("invalid input");
+  }
   try {
     const { email } = req.body;
     const { id } = req.user;
-  
-  await orgService.createOrganizationInvite(email, id);
+
+    await orgService.createOrganizationInvite(email, id);
     return res.status(200).json({
       message: "success",
       statusCode: 200,
@@ -60,8 +59,49 @@ const sendOrganizationInvite = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+const updateLunchPrice = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { org_id, is_admin } = await User.findOne({ where: { id: id } });
+    if (!is_admin) {
+      return res.status(400).json({
+        message: "Unauthorised",
+        statusCode: 400,
+        data: null,
+      });
+    }
+    const { lunch_price } = req.body;
+    if (typeof lunch_price === string) {
+      return res.status(400).json({
+        message: "Invalid Price",
+        statusCode: 400,
+        data: null,
+      });
+    }
+    const checkOrg = await Organization.findOne({ where: { id: org_id } });
+    if (!checkOrg) {
+      return res.status(200).json({
+        message: "No Organization found",
+        statusCode: 400,
+        data: null,
+      });
+    }
+    await Organization.update({ lunch_price }, { where: { id: org_id } });
+    return res.status(200).json({
+      message: "success",
+      statusCode: 200,
+      data: null,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: `Lunch Price Update Failed: ${error?.message}`,
+      statusCode: 500,
+      data: null,
+    });
+  }
+};
 module.exports = {
   createOrUpdateOrg,
   sendOrganizationInvite,
+  updateLunchPrice,
 };
-
--- routes/api/organization.js
+++ routes/api/organization.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const orgController = require('../../controllers/organization');
+const {createOrUpdateOrg,sendOrganizationInvite, updateLunchPrice} = require('../../controllers/organization');
 const authenticateToken = require('../../middleware/user');
 const { body } = require('express-validator');
 
-router.put('/create/:orgId', authenticateToken, orgController.createOrUpdateOrg);
-router.post("/invite",[body("email").isEmail().isEmpty()], authenticateToken, orgController.sendOrganizationInvite);
+router.put('/create/:orgId', authenticateToken, createOrUpdateOrg);
+router.post("/invite",[body("email").isEmail().isEmpty()], authenticateToken, sendOrganizationInvite);
+router.patch("/lunch/update", authenticateToken, updateLunchPrice );
 module.exports = router;
