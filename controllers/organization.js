9b0b358e5f1bb7fdb86d01979e754dbdebb3f681
ignatiusomeje <mrexcel153@gmail.com>
2023-09-23 11:37:21 +0100

    add a check to know if amount exist and change the string check to a string. format the document and remove commented out codes.

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,13 +1,11 @@
-const signupStaff = require("../services/staff").signupStaff
-const jwt = require("jsonwebtoken")
+const signupStaff = require("../services/staff").signupStaff;
+const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
 const orgService = require("../services/organization");
 const { Organization, Organization_lunch_wallet } = require("../models");
 const { User } = require("../models");
-const decryptEncryptedOTP = require("../utils/helpers").decryptEncryptedOTP
+const decryptEncryptedOTP = require("../utils/helpers").decryptEncryptedOTP;
 const { string } = require("yargs");
-
-
 
 const createOrUpdateOrg = async (req, res) => {
   const { id } = req.user;
@@ -24,92 +22,65 @@ const createOrUpdateOrg = async (req, res) => {
       message: "Organization created or updated successfully",
       organization,
     });
-
-    //check if orgId is provided (indicating an update) if not (indicate creation)
-    // const orgId = req.params.orgId; //Assuming if orgId is passed in the url
-
-    // if (orgId) {
-    //   //if orgId is provided it is treated as creation
-    //   const updateOrg = await orgService.updateOrganization(id, {
-    //     organization_name,
-    //     lunch_price,
-    //   });
-
-    //   return res.status(200).json({
-    //     message: "Organization created or updated successfully",
-    //     updateOrg,
-    //   });
-    // } else {
-    //   //if orgId is not provided it is treated as creation
-    //   const createOrg = await orgService.createOrganization({
-    //     organization_name,
-    //     lunch_price,
-    //   });
-
-    //   return res.status(201).json({
-    //     message: "Organization created or updated successfully",
-    //     createOrg,
-    //   });
-    // }
   } catch (err) {
-    console.log("Error creating or updating organization", err);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
+const staffSignup = async (req, res) => {
+  try {
+    const { email, password, otp_token, first_name, last_name, phone_number } =
+      req.body;
 
-const staffSignup = async (req, res)=>{
-    try {
-          const  {email, password, otp_token, first_name, last_name, phone_number} = req.body
+    if (!phone_number) {
+      return res.status(400).json({ error: "missing phone number" });
+    }
+    if (!email) {
+      return res.status(400).json({ error: "missing email" });
+    }
+    if (!otp_token) {
+      return res.status(400).json({ error: "missing otp_token" });
+    }
+    if (!first_name) {
+      return res.status(400).json({ error: "missing first_name" });
+    }
+    if (!last_name) {
+      return res.status(400).json({ error: "missing last name" });
+    }
+    if (!password) {
+      return res.status(400).json({ message: "missing password" });
+    }
 
-      if (!phone_number){
-            return res.status(400).json({error: "missing phone number"})
-          }
-	  if (!email){
-		  return res.status(400).json({error: "missing email"})
-          }
-	  if (!otp_token){
-		  return res.status(400).json({error: "missing otp_token"})}
-	  if (!first_name){
-		  return res.status(400).json({error: "missing first_name"})
-          }
-      if (!last_name){
-            return res.status(400).json({error: "missing last name"})
-          }if (!password){
-		  return res.status(400).json({message: "missing password"})
-	  }
+    // get organization id from token
+    const orgId = decryptEncryptedOTP(otp_token);
 
-        // get organization id from token
-        const orgId = decryptEncryptedOTP(otp_token)
-         
-          // Call the signupStaff service function
-          const user  = await signupStaff({
-          email,
-          password,
-          orgId,
-          first_name,
-          last_name,
-          phone_number
-      });
-      if (!user) return res.status(400).json({"error": "Email already in use"})
+    // Call the signupStaff service function
+    const user = await signupStaff({
+      email,
+      password,
+      orgId,
+      first_name,
+      last_name,
+      phone_number,
+    });
+    if (!user) return res.status(400).json({ error: "Email already in use" });
 
-      console.log(user)
-          const formattedUser = {
-          id: user.id,
-          email: user.email,
-          org_id: user.org_id,
-          first_name: user.first_name,
-          last_name: user.last_name,
-	        phone_number: user.phone_number,
-          created_at: user.created_at,
-          updated_at: user.updated_at,
-      };
-      
-    res.status(201).json({ success: true, user: formattedUser});
+    const formattedUser = {
+      id: user.id,
+      email: user.email,
+      org_id: user.org_id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      phone_number: user.phone_number,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    res.status(201).json({ success: true, user: formattedUser });
   } catch (error) {
-    res.status(403).json({"error": "invalid token"});
+    res.status(403).json({ error: "invalid token" });
   }
-}
+};
 
 const sendOrganizationInvite = async (req, res) => {
   const errors = validationResult(req.body);
@@ -130,7 +101,6 @@ const sendOrganizationInvite = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
-
 
 const updateLunchPrice = async (req, res) => {
   try {
@@ -174,7 +144,6 @@ const updateLunchPrice = async (req, res) => {
   }
 };
 
-
 const updateWalletBalance = async (req, res) => {
   try {
     const { id } = req.user;
@@ -186,16 +155,25 @@ const updateWalletBalance = async (req, res) => {
         data: null,
       });
     }
-   
+
     const { amount } = req.body;
-    if (typeof amount === string) {
+    if (!amount){
+      return res.status(400).json({
+        message: "No amount given",
+        statusCode: 400,
+        data: null,
+      });
+    }
+    if (typeof amount === "string") {
       return res.status(400).json({
         message: "Invalid Price",
         statusCode: 400,
         data: null,
       });
     }
-    const checkOrg = await Organization_lunch_wallet.findOne({ where: { org_id } });
+    const checkOrg = await Organization_lunch_wallet.findOne({
+      where: { org_id },
+    });
     if (!checkOrg) {
       return res.status(400).json({
         message: "No Organization found",
@@ -203,7 +181,10 @@ const updateWalletBalance = async (req, res) => {
         data: null,
       });
     }
-    await Organization_lunch_wallet.update({ balance: amount }, { where: {  org_id } });
+    await Organization_lunch_wallet.update(
+      { balance: amount },
+      { where: { org_id } }
+    );
     return res.status(200).json({
       message: "success",
       statusCode: 200,
@@ -223,6 +204,5 @@ module.exports = {
   sendOrganizationInvite,
   staffSignup,
   updateLunchPrice,
-  updateWalletBalance
+  updateWalletBalance,
 };
-
